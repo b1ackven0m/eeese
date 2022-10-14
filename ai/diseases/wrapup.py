2ea97d3ddeb98b308be87fb8f60ddccfe83d1444
b1ackven0m <114716239+b1ackven0m@users.noreply.github.com>
2022-10-15 01:33:41 +0200

    done AI

--- ai/diseases/wrapup.py
+++ ai/diseases/wrapup.py
@@ -84,35 +84,32 @@ algo_time['K-NEAREST NEIGHBOURS']=end-start
 
 
 
-print("COVID PREDICTION BASED ON ML ALGORITHMS")
-print("Enter 1 for Yes and 0 for No")
-Breathing_Problem = int(input("Does the patient have breathing problem ? "))
-Fever = int(input("Does the patient have fever ? "))
-Dry_Cough = int(input("Does the patient have dry cough ? "))
-Sore_throat = int(input("Does the patient have sore throat ? "))
-Running_Nose = int(input("Does the patient have running nose ? ")) 
-Asthma = int(input("Does the patient have any record of asthma ? ")) 
-Chronic_Lung_Disease = int(input("Does the patient have any records of chronic lung disease ? ")) 
-Headache = int(input("Is the patient having headche ? "))
-Heart_Disease = int(input("Does the patient have any record of any heart disease ? "))
-Diabetes = int(input("Does the patient have diabetes ? "))
-Hyper_Tension = int(input("Does the patient have hyper tension ? ")) 
-Fatigue = int(input("Does the patient experience fatigue ? "))
-Gastrointestinal = int(input("Does the patient have any gastrointestinal disorders ? "))
-Abroad_travel = int(input("Has the patient travelled abroad recently ? "))
-Contact_with_COVID_Patient = int(input("Was the patient in contact with a covid patient recently ? "))
-Attended_Large_Gathering = int(input("Did the patient attend any large gathering event recently ? "))
-Visited_Public_Exposed_Places = int(input("Did the patient visit any public exposed places recently ? "))
-Family_working_in_Public_Exposed_Places = int(input("Does the patient have any family member working in public exposed places ? "))
-test1 = int(input("test1?"))
-test2 = int(input("test1?"))
-patient = [[test1,test2,Breathing_Problem,Fever,Dry_Cough,Sore_throat,Running_Nose,Asthma,Chronic_Lung_Disease,Headache,Heart_Disease,Diabetes,Hyper_Tension,Fatigue,Gastrointestinal,Abroad_travel,Contact_with_COVID_Patient,Attended_Large_Gathering,Visited_Public_Exposed_Places,Family_working_in_Public_Exposed_Places]]
-result = knn_cv.predict(patient)
-print("\nResults : ",result)
+# print("COVID PREDICTION BASED ON ML ALGORITHMS")
+# print("Enter 1 for Yes and 0 for No")
+# Breathing_Problem = int(input("Does the patient have breathing problem ? "))
+# Fever = int(input("Does the patient have fever ? "))
+# Dry_Cough = int(input("Does the patient have dry cough ? "))
+# Sore_throat = int(input("Does the patient have sore throat ? "))
+# Running_Nose = int(input("Does the patient have running nose ? ")) 
+# Asthma = int(input("Does the patient have any record of asthma ? ")) 
+# Chronic_Lung_Disease = int(input("Does the patient have any records of chronic lung disease ? ")) 
+# Headache = int(input("Is the patient having headche ? "))
+# Heart_Disease = int(input("Does the patient have any record of any heart disease ? "))
+# Diabetes = int(input("Does the patient have diabetes ? "))
+# Hyper_Tension = int(input("Does the patient have hyper tension ? ")) 
+# Fatigue = int(input("Does the patient experience fatigue ? "))
+# Gastrointestinal = int(input("Does the patient have any gastrointestinal disorders ? "))
+# Abroad_travel = int(input("Has the patient travelled abroad recently ? "))
+# Contact_with_COVID_Patient = int(input("Was the patient in contact with a covid patient recently ? "))
+# Attended_Large_Gathering = int(input("Did the patient attend any large gathering event recently ? "))
+# Visited_Public_Exposed_Places = int(input("Did the patient visit any public exposed places recently ? "))
+# Family_working_in_Public_Exposed_Places = int(input("Does the patient have any family member working in public exposed places ? "))
+# test1 = int(input("test1?"))
+# test2 = int(input("test1?"))
+# patient = [[test1,test2,Breathing_Problem,Fever,Dry_Cough,Sore_throat,Running_Nose,Asthma,Chronic_Lung_Disease,Headache,Heart_Disease,Diabetes,Hyper_Tension,Fatigue,Gastrointestinal,Abroad_travel,Contact_with_COVID_Patient,Attended_Large_Gathering,Visited_Public_Exposed_Places,Family_working_in_Public_Exposed_Places]]
 
-if result == 1:
-    print(Fore.RED + 'You may be affected with COVID-19 virus! Please get RTPCR test ASAP and stay in Quarantine for 14 days!')
-    print()
-else :
-    print(Fore.GREEN + 'You do not have any symptoms of COVID-19. Stay home! Stay safe!')
-    print()
+
+
+
+def result(data):
+    return knn_cv.predict(data)
--- ai/main.py
+++ ai/main.py
@@ -1,12 +1,22 @@
-from typing import Union
-
 from fastapi import FastAPI
-
+import json
+from pydantic import BaseModel
+from diseases.wrapup import *
+import numpy as np
 app = FastAPI()
 
+class Symptoms(BaseModel):
+    symptoms:list[int]
 
 @app.get("/")
 def root():
-    return {"Hello": "World"}
+    covid = ["Does the patient have breathing problem ? ","Does the patient have fever ?","Does the patient have dry cough ?","Does the patient have any record of asthma ?","Does the patient have any records of chronic lung disease ?"]
+    return covid
 
-app.listen(800)
+@app.post("/")
+def isInfected(data : Symptoms):
+    test = data.symptoms
+
+    if result([test])[0] == 1 :
+        return 1
+    return 0
